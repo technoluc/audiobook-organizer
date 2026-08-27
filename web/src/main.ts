66434fde34de16c28f817787c65ff58281a75a42
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2026-08-27 11:56:45 +0200

    fix(web): restore persisted UI state on startup

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css'
+import { installWebStatePersistence } from './statePersistence'
 
 createApp(App).mount('#app')
+installWebStatePersistence()
